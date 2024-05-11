473341d10c0196f54df2114e3f35cad79e3dd33e
Ozan Cinci <ozan_cinci2001@hotmail.com>
2024-05-11 18:11:53 +0300

    last checkpoint before implementing cache

---
+++ src/main/java/com/ozan/be/externalProducts/ExternalProductsController.java
@@ -0,0 +1,14 @@
+package com.ozan.be.externalProducts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/external-products")
+@RequiredArgsConstructor
+public class ExternalProductsController {
+
+
+
+}
